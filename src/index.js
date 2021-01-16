433e501f6d1b06654aa6db3fae344b0f3b23a98e
Rían Errity <rian@paradaux.io>
2021-01-16 13:26:33 +0000

    Footer advertisement

--- src/index.js
+++ src/index.js
@@ -206,7 +206,10 @@ let renderDrama = (message, share, sharePath, teaser) => {
         </main>
 
         <footer>
-            <a href="https://syscraft.org" class="text-center text-muted">Syscraft is fully endorsed by LLS/LLM Conglomerate.</a>
+            <p class="text-center text-muted"><a href="https://syscraft.org" class="text-center">Syscraft</a> is fully 
+            endorsed by the <a href="https://larrylovesquad.com">LLS</a>/<a href="https://larrylynchmob.com">LLM</a> 
+            Conglomerate. | We have an <a href="https://larrygenerator.com/api/generate">API</a>! | Contribute on 
+            <a href="https://github.com/LLS-LLM/larrygenerator">GitHub!</a></p>
         </footer>
     </body>
 
